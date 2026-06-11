3f68274addd151c5d0818468426de329a097718c
MOHAMMED IRFAN <mohammed.irfan3@ibm.com>
2026-06-11 16:26:54 +0530

    Activity 7 completed

--- FST_Selenium_Project/src/test/java/project/Activity7.java
+++ FST_Selenium_Project/src/test/java/project/Activity7.java
@@ -1,12 +1,23 @@
 package project;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import java.awt.Desktop.Action;
 import java.time.Duration;
+import java.util.List;
+import java.util.function.BooleanSupplier;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Activity7 {
 	
@@ -15,13 +26,91 @@ public class Activity7 {
 	//Declare Explicitwait
 	WebDriverWait wait;
 	//Declare Action
-	Action builder;
+	Actions builder;
 	
 	@BeforeClass
 	public void setUp() {
+		//Initialize object for the driver
 		driver = new FirefoxDriver();
+		//Initialize object for the wait		
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		builder = new Acti
+		//Initialize object for the action	
+		builder = new Actions(driver);
+	}
+	
+	@Test(priority = 1)
+	public void Login_Page() {
+		//Open the url
+		driver.get("http://hrm.local:3050/symfony/web/index.php/auth/login#/Login");
+		//Checks its on the right page
+		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+		//Locate user id element and passing user id
+		driver.findElement(By.id("txtUsername")).sendKeys("orange");
+		//Locate password element and passing user id
+		driver.findElement(By.id("txtPassword")).sendKeys("5Nx#I6BK%r3$8vz0ch");
+		//click the login button
+		driver.findElement(By.id("btnLogin")).click();
+		//Checks redirected to the welcome page
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String welcome_admin = driver.findElement(By.id("welcome")).getText();
+		Assert.assertEquals(welcome_admin, "Welcome Admin");
+	}
+	
+	@Test(priority=2)
+	public void myInfo() {
+		//locate My_Info element and click it
+		WebElement my_info= driver.findElement(By.xpath("//b[text()='My Info']"));
+		builder.click(my_info).pause(2000).build().perform();
+		
+		//Checks it is on Myinfo Page
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String Personal_details=driver.findElement(By.xpath("//h1[text()='Personal Details']")).getText();
+		Assert.assertEquals(Personal_details, "Personal Details");
+	}
+	
+	@Test(priority=3)
+	public void qualification() {
+		//locate  qualification and click it
+		driver.findElement(By.linkText("Qualifications")).click();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		Assert.assertEquals(driver.getCurrentUrl(),"http://hrm.local:3050/symfony/web/index.php/pim/viewQualifications/empNumber/1");
+	}
+	
+	@Test(priority=4)
+	public void addWork() {
+		//locate AddWork button and click it
+		driver.findElement(By.id("addWorkExperience")).click();
+		// checks it showing the fields to fill
+		assertTrue(driver.findElement(By.id("experience_employer")).isDisplayed());
+		//locate company field and enter value
+		driver.findElement(By.id("experience_employer")).sendKeys("IBM");
+		//locate job title field and enter value
+		driver.findElement(By.id("experience_jobtitle")).sendKeys("ASE");
+		//locate Date from field and enter value
+		driver.findElement(By.id("experience_from_date")).clear();
+		driver.findElement(By.id("experience_from_date")).sendKeys("2025-10-02");
+		//locate Date to field and enter value
+		driver.findElement(By.id("experience_to_date")).clear();
+		driver.findElement(By.id("experience_to_date")).sendKeys("2026-04-15");
+		//locate comment field enter value
+		driver.findElement(By.id("experience_comments")).sendKeys("nothing ");
+		//locate save button and click it
+		driver.findElement(By.id("btnWorkExpSave")).click();
+		//check it is added to the work experience table
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		Boolean is_present = false;
+		List<WebElement>  worklist = driver.findElements(By.xpath("//table/tbody/tr/td"));
+		for(WebElement row :worklist) {
+			if(row.getText().contains("IBM")) {
+				is_present = true;
+			}
+		}
+		
+		Assert.assertTrue(is_present);
+	}
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
 	}
 
 }
