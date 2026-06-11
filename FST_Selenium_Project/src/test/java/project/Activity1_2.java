6e8af8982e98011466aa321614464f951aa966e4
Abhishek Kesharwani <abhishek.kesharwani@ibm.com>
2026-06-11 16:47:49 +0530

    Activity 1 & 2 completed

---
+++ FST_Selenium_Project/src/test/java/project/Activity1_2.java
@@ -0,0 +1,66 @@
+package orange_hrm_test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class Activity1_2 {
+	// Declare the driver
+	WebDriver driver;
+
+	@Parameters({"browserName"})
+	@BeforeClass
+	public void setUp(String browserName) {
+		// Initialize the driver
+		switch (browserName) {
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		default:
+			driver = null;
+		}
+
+		// Open a browser and navigate to
+		// http://hrm.local:3050/symfony/web/index.php/auth/login
+		driver.get("http://hrm.local:3050/symfony/web/index.php/auth/login");
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	}
+
+	// G
+	@Test(priority = 1)
+	public void getTitle() {
+		String title = driver.getTitle();
+		Assert.assertEquals("OrangeHRM", title);
+	}
+
+	@Test(dependsOnMethods = "getTitle")
+	public void getHeaderImageUrl() {
+		WebElement image = driver.findElement(By.xpath("//img[contains(@src, 'symfony')]"));
+		String src = image.getAttribute("src");
+		System.out.println(src);
+	}
+
+	@AfterClass
+	public void tearDown() {
+		// close the browser
+		driver.quit();
+	}
+}
